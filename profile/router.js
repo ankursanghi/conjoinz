e5ae865c281a756a34ff1d35dedd9911f790c0d1
Ankur Sanghi <ankur.sanghi@gmail.com>
2015-11-09 23:41:28 -0500

    redone the profile save with async

--- profile/router.js
+++ profile/router.js
@@ -3,6 +3,7 @@ var Address = require('../models/address.js');
 var User = require('../models/user.js');
 var bodyParser = require('body-parser');
 var router = new express.Router();
+var async = require('async');
 
 router.use(bodyParser());
 
@@ -34,12 +35,6 @@ function updateProfile(req, res, next){
 	address.phone = req.body.billing.phone;
 	//build details for querying the address collection
 	var query = {};
-//	query.adr_type = 'Primary';
-//	query.adr_line1 = req.body.billing['address'][0];
-//	query.adr_line2 = req.body.billing['address'][1];
-//	query.city = req.body.billing.city;
-//	query.state = req.body.billing.state;
-//	query.zip = req.body.billing.zipcode;
 	console.log('query adr_nick is:'+query.adr_nick);
 	var userQuery = {};
 	userQuery.name = {};
@@ -49,62 +44,93 @@ function updateProfile(req, res, next){
 	var user = {};
 	// query.adr_nick = req.body.adrnick || req.body.address; // req.body.address is the selection from the dropdown
 	query.adr_nick = req.body.address; // req.body.address is the selection from the dropdown
-	nickUsed = false;
-	if (req.body.address != '9new9') {
-		query.adr_nick = req.body.address;
-	}else{
-		// check if this nick has been used already	
-		User.findOne(userQuery).populate('delivery_addresses').exec(function(err, user){
-			console.log('user found in profile page logic:'+JSON.stringify(user));
-			user.delivery_addresses.forEach(function(element){
-				if (element.adr_nick == req.body.adrnick){
-					console.log('error condition about nick met');
-					nickUsed = true;
-				}
-			});
-		});
-	}
-	//find one and update makes sure that if there is a change in the address you could save it.
-	Address.findOneAndUpdate(query,address,options,function(err, addrSaved){
-		if (err) {
-		    console.log('Error Inserting New Data');
-		    console.log('error as-is:'+JSON.stringify(err));
-		    if (err.name == 'ValidationError') {
-			    for (field in err.errors) {
-				    console.log(err.errors[field].message); 
-			    }
-		    }
-		}
-		console.log('saved the line #'+addrSaved);
-		var findUserQuery = User.findOne(userQuery);
-		findUserQuery.populate('delivery_addresses').exec(function(err, usr){
-			var found = false;
-			usr.delivery_addresses.forEach(function(adr){
-				// if (adr.adr_nick == address.adr_nick && adr.adr_line1 == address.adr_line1 && adr.city == address.city && adr.zip == address.zip){
-				console.log('adr_nick from user:'+adr.adr_nick);
-				if (adr.adr_nick == address.adr_nick){ // if the address nick from the form is equal to one of the addresses in the user's delivery addresses
-					found = true;
-				}
-			});
-			if (!found){
-				User.findOneAndUpdate(userQuery, {"$push":{"delivery_addresses": addrSaved._id}}, options, function(err, userUpdated){
-					if (err) {
-					    console.log('Error Inserting New Data');
-					    if (err.name == 'ValidationError') {
-						    for (field in err.errors) {
-							    console.log(err.errors[field].message); 
-						    }
-					    }
-					}
-					console.log("User updated with new address");
-				});	
+	updateAddress = false;
+	// async waterfall here
+	async.waterfall([
+		function(callback){
+			var udpateAddress = false;
+			if (req.body.address != '9new9') {
+				updateAddress = true;
+				return callback(null, updateAddress);
 			}else{
-				addrSaved.adr_nick = false; //nulling out address nick in the addrSaved to display error on UI
+				// check if the new nick selected is the same as one of the nicks saved on this profile
+				var findUserQuery = User.findOne(userQuery);
+				findUserQuery.populate('delivery_addresses').exec(function(err, usr){
+					var found = false;
+					usr.delivery_addresses.forEach(function(adr){
+						// if (adr.adr_nick == address.adr_nick && adr.adr_line1 == address.adr_line1 && adr.city == address.city && adr.zip == address.zip){
+						console.log('adr_nick from user:'+adr.adr_nick);
+						if (adr.adr_nick == req.body.adrnick){ // if the address nick from the form is equal to one of the addresses in the user's delivery addresses
+							found = true;
+							var err = new Error('nickUsed');
+							return callback(err, null);
+						}
+					});
+				});
+				return callback(null, updateAddress);
 			}
-			
-			res.render("profile/profile", {layout: false, error:found, saved: addrSaved.adr_nick, name: req.session.name});
+		},
+		function (updateAddress, callback){
+			console.log('determined update address:'+updateAddress);
+			if (updateAddress){
+				//find id of the delivery address that has a matching nick on this user	
+				// update that address with the details
+				User.findOne(userQuery).populate('delivery_addresses').exec(function(err, user){
+					console.log('user found in profile page logic:'+JSON.stringify(user));
+					user.delivery_addresses.forEach(function(element){
+						if (element.adr_nick == req.body.address){
+							console.log('error condition about nick met');
+							query._id = element._id;
+							Address.findOneAndUpdate(query, address, options, function(err, addrSaved){
+								if (err) {
+								    console.log('Error Inserting New Data');
+								    console.log('error as-is:'+JSON.stringify(err));
+								    if (err.name == 'ValidationError') {
+									    for (field in err.errors) {
+										    console.log(err.errors[field].message); 
+									    }
+								    }
+								    return callback(err, null, null);
+								}
+								return callback(null, updateAddress, addrSaved);
+							});
+						}
+					});
+				});
+			}else{
+				// here we know we have to save a new Address
+				var newAddress = new Address(address);
+				newAddress.save(function(err, addrSaved){
+					if (err){
+						return callback(err, null, null);
+					}else{
+						User.findOneAndUpdate(userQuery, {"$push":{"delivery_addresses": addrSaved._id}}, options, function(err, userUpdated){
+							if (err) {
+							    console.log('Error Inserting New Data');
+							    if (err.name == 'ValidationError') {
+								    for (field in err.errors) {
+									    console.log(err.errors[field].message); 
+								    }
+							    }
+							    return callback(err, null, null);
+							}
+							console.log("User updated with new address");
+						});	
+						return callback(null, updateAddress, addrSaved);
+					}
+				});
+			}
+		},
+		function(updated, addr, callback){
+			callback(null, addr.adr_nick);	
+		}
+		], function(err, result){
+			var found;
+			if (err && err.message == 'nickUsed'){
+				found = true;
+			}
+			res.render("profile/profile", {layout: false, error:found, saved: result, name: req.session.name});
 		});
-	});	
 }
 
 router.get("/profile",profile);
