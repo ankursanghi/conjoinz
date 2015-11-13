75ed8737ea914f7440807326d49a1aafe267c8f4
Ankur Sanghi <ankur.sanghi@gmail.com>
2015-11-13 17:54:11 -0500

    added login status to faqs and about pages

--- faqs/router.js
+++ faqs/router.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = new express.Router();
 
 function faqs(req, res) {
-	  res.render("faqs/faqs", {layout: false});
+	  res.render("faqs/faqs", {layout: false, name: req.session.name});
 }
 
 router.get("/faqs", faqs);
