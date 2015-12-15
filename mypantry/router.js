272260ac64cf68b3bb7bbcec09f3412d50e4775c
Thanuja <thanuja@kenturf.com>
2015-12-15 15:17:03 +0530

    pantry page alert msg updated

--- mypantry/router.js
+++ mypantry/router.js
@@ -9,6 +9,7 @@ var router = new express.Router();
 var multer = require('multer');
 var upload = multer({dest: './public/img/'});
 var orderApi = require("../api/order/router.js");
+var pantryApi = require("../api/pantry/router.js");
 var moment = require("moment");
 router.use(bodyParser());
 router.use(upload.array());
@@ -17,7 +18,9 @@ function savePantry(req, res, next){
 	if (!(req.connection.encrypted)){
 	return res.redirect("https://" + req.headers.host.replace('8008','8009') + req.url);
 	}
-
+	var emptyOrderItem = req.body.ord_number;
+	var status = req.body.deleteitem;
+	var upcstatus = req.body.upc;
 	var first = req.session.name.split(" ")[0];
 	var last = req.session.name.split(" ")[1];
 	var pantryDetails={
@@ -25,54 +28,66 @@ function savePantry(req, res, next){
 		brand:"",
 		comments:""
 	};
-
-	var status = req.body.deleteitem;
-	
-	if(status=="delete"){
-		if (req.body.upc instanceof Array){
-			req.body.upc.forEach(function (upc, Index){
-				var upc=req.body.upc[Index];
-				Pantry.update({email:req.session.user},{$pull:{productsArray:{upc:upc}}}, { multi: true},function(err,pantry){
-					if(err){
-						console.log("values not avilable in database");
-					}
-					//console.log('pantry '+JSON.stringify(pantry));
-					Pantry.find({email:req.session.user},function(err,pantryDetails){
+	if(emptyOrderItem == "empty" && status != "delete"){
+		Pantry.findOne({email:req.session.user},function(err,pantryDetails){
 						if(err){
 							console.log("values not avilable in database");
 						}
-					//	console.log('pantryDetails  in db ****'+JSON.stringify(pantryDetails));
-
 						Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
 							if(err){
 								console.log("values not avilable in orders dable");
 							}
-							//console.log('orderss '+JSON.stringify(orderss));
-							res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails , order:orderss, status:true});
-						});
-						//res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails , value:true});
-					});
+							res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails ,order:orderss, emptyOrderItem:true});
+							});
+		});
+		
+	}
+	
+	console.log("upcstatus"+upcstatus);
+	if(!(upcstatus)){
+		Pantry.findOne({email:req.session.user},function(err,pantryDetails){
+						if(err){
+							console.log("values not avilable in database");
+						}
+						Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
+							if(err){
+								console.log("values not avilable in orders dable");
+							}
+		//console.log("upcstatus###",+upcstatus);
+					if(status=="delete")
+					{
+						res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails ,order:orderss, upcstatus:true});
+					}
+					else
+					{
+						var addItem = status;
+						res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails ,order:orderss,addItem:true});
+					}
+		});
+		});
+
+	}else
+	{
+	//console.log("upcstatus####"+upcstatus);
+	if(status=="delete"){
+		if (req.body.upc instanceof Array){
+			req.body.upc.forEach(function (upc, Index){
+				var email = req.session.user;
+				var upc = req.body.upc[Index];
+				Pantry.update({email:req.session.user},{$pull:{productsArray:{upc:upc}}}, { multi: true},function(err,pantry){
+					if(err){
+						console.log("values not avilable in database");
+					}
 				});
 			});
 		}
 		else{
 			var upc=req.body.upc;
+			var email = req.session.user;
 			Pantry.update({email:req.session.user},{$pull:{productsArray:{upc:upc}}}, { multi: true},function(err,pantriees){
 				if(err){
 					console.log("values not avilable in database");
 				}
-			});
-			Pantry.findOne({email:req.session.user},function(err,pantryDetails){
-				if(err){
-					console.log("values not avilable in database");
-				}
-				Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
-					if(err){
-						console.log("values not avilable in orders dable");
-					}
-					//console.log('orderss  page ***'+JSON.stringify(orderss));
-					res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails, order:orderss, status:true});
-				});
 			});
 		}
 		//delete option ending
@@ -89,11 +104,7 @@ function savePantry(req, res, next){
 		var order_number="";
 		var now = moment(new Date());
 		var orderDate = now.format("YYYY MMM DD HH:mm");
-
-		
 		var order_number= req.body.ord_number; 
-		
-					
 		console.log("values printed +" +order_number);
 		if(order_number==" "){
 			console.log("values printed ++" +order_number);
@@ -125,40 +136,23 @@ function savePantry(req, res, next){
 					brand:"",
 					comments:""
 				};
-
-
 				Order.create(order, function(err, newOrder){
-					// added the new values in db
 					new_order_number=newOrder.ord_number;
 					if (req.body.upc instanceof Array){
 						async.forEachOf(req.body.upc, function (upc, Index){
 							var upc=req.body.upc[Index];
-							//console.log(" avilable in database  ***" +req.session.user + "," +Index);
 							Pantry.findOne({"productsArray.upc":upc,"email":req.session.user},function(err,pantry){
 								if(err){
 									console.log("values not avilable in database");
-								}
-									//console.log(" avilable in database #" +pantry.productsArray[Index].product_name);
+								}	
 								Item.findOne({"name": pantry.productsArray[Index].product_name}, function(err, item){ 	
 									pantryDetails.orderItem=item._id;
 									pantryDetails.brand=pantry.productsArray[Index].brand;
 									pantryDetails.comments=pantry.productsArray[Index].product_description;
-									//console.log('after item pantry '+JSON.stringify(pantryDetails));
 									Order.findOneAndUpdate({"ord_number":new_order_number},{"$push": {"ord_lines": pantryDetails}}, function(err,orders){
 										if(err){
 											console.log("values not avilable in orders dable");
 										}
-										Pantry.findOne({"email":email},function(err,pantries){
-											if(err){
-												console.log("values not avilable in database");
-											}
-											Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
-												if(err){
-													console.log("values not avilable in orders dable");
-												}
-												res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantries ,order:orderss, value:true});
-											});
-										});
 									});
 								});
 							});
@@ -167,33 +161,19 @@ function savePantry(req, res, next){
 
 						var order_number= req.body.ord_number; 
 						var upc=req.body.upc;
-						//console.log(" avilable in database test ***" +req.session.user );
 						Pantry.findOne({"productsArray.upc":upc,"email":req.session.user},function(err,pantry){
 							if(err){
 								console.log("values not avilable in database");
 							}
-							//console.log(" avilable in database #" +pantry.productsArray[0].product_name);
 							Item.findOne({"name": pantry.productsArray[0].product_name}, function(err, item){ 	
 								pantryDetails.orderItem=item._id;
 								pantryDetails.brand=pantry.productsArray[0].brand;
 								pantryDetails.comments=pantry.productsArray[0].product_description;
-								//console.log('after item pantry '+JSON.stringify(pantryDetails));
 								Order.findOneAndUpdate({"ord_number":new_order_number},{"$push": {"ord_lines": pantryDetails}}, function(err,orders){
 									if(err){
 										console.log("values not avilable in orders dable");
 									}
-									Pantry.findOne({"email":email},function(err,pantries){
-										if(err){
-											console.log("values not avilable in database");
-										}
-										//console.log('pantry '+JSON.stringify(pantry));
-										Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
-											if(err){
-												console.log("values not avilable in orders dable");
-											}
-											res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantries ,order:orderss,value:true});
-										});
-									});
+									
 								});
 							});
 						});
@@ -207,32 +187,19 @@ function savePantry(req, res, next){
 					var order_number= req.body.ord_number; 
 					async.forEachOf(req.body.upc, function (upc, Index){
 					var upc=req.body.upc[Index];
-						//console.log(" avilable in database  ***" +req.session.user + "," +Index);
 						Pantry.findOne({"productsArray.upc":upc,"email":req.session.user},function(err,pantry){
 							if(err){
 								console.log("values not avilable in database");
 							}
-							//console.log(" avilable in database #" +pantry.productsArray[Index].product_name);
 							Item.findOne({"name": pantry.productsArray[Index].product_name}, function(err, item){ 	
 								pantryDetails.orderItem=item._id;
 								pantryDetails.brand=pantry.productsArray[Index].brand;
 								pantryDetails.comments=pantry.productsArray[Index].product_description;
-								//console.log('after item pantry '+JSON.stringify(pantryDetails));
 								Order.findOneAndUpdate({"ord_number":order_number},{"$push": {"ord_lines": pantryDetails}}, function(err,orders){
 									if(err){
 										console.log("values not avilable in orders dable");
 									}
-									Pantry.findOne({"email":email},function(err,pantries){
-										if(err){
-											console.log("values not avilable in database");
-										}
-										Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
-											if(err){
-												console.log("values not avilable in orders dable");
-											}
-											res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantries ,order:orderss, value:true});
-										});
-									});
+									
 								});
 								
 
@@ -244,44 +211,50 @@ function savePantry(req, res, next){
 					//start with order number witout array
 					var order_number= req.body.ord_number; 
 					var upc=req.body.upc;
-					//console.log(" avilable in database test ***" +req.session.user );
 					Pantry.findOne({"productsArray.upc":upc,"email":req.session.user},function(err,pantry){
 						if(err){
 							console.log("values not avilable in database");
 						}
-						//console.log(" avilable in database #" +pantry.productsArray[0].product_name);
 						Item.findOne({"name": pantry.productsArray[0].product_name}, function(err, item){ 	
 							pantryDetails.orderItem=item._id;
 							pantryDetails.brand=pantry.productsArray[0].brand;
 							pantryDetails.comments=pantry.productsArray[0].product_description;
-							//console.log('after item pantry '+JSON.stringify(pantryDetails));
 							Order.findOneAndUpdate({"ord_number":order_number},{"$push": {"ord_lines": pantryDetails}}, function(err,orders){
 								if(err){
 									console.log("values not avilable in orders dable");
 								}
-								Pantry.findOne({"email":email},function(err,pantries){
-									if(err){
-										console.log("values not avilable in database");
-									}
-									//console.log('pantry '+JSON.stringify(pantry));
-									Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
-										if(err){
-											console.log("values not avilable in orders dable");
-										}
-										res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantries ,order:orderss,value:true});
-									});
-								});
+								
 							});
 						});
 					});
 //end with order number array
 				}
 
-
 		}
+
+
+
+	}
+
+	Pantry.findOne({email:req.session.user},function(err,pantryDetails){
+						if(err){
+							console.log("values not avilable in database");
+						}
+						Order.find({"customer.name.first":first,"customer.name.last":last,"ord_status":"saved" },function(err,orderss){
+							if(err){
+								console.log("values not avilable in orders dable");
+							}
+						if(status=="delete"){
+							res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails , order:orderss, status:true});
+						}
+						else
+						{
+							res.render("pantry/pantry", {layout: false, name:req.session.name , pantry:pantryDetails ,order:orderss, value:true});
+						}
+						});
+					});
 	}
 }
-
 router.post("/addtoOrder",savePantry);
 
 module.exports = router;
